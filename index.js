86ee21aa67a9726d31e35e3432f5dc188a6f3b09
Ogun.B <146710989+OgunBiciciToster@users.noreply.github.com>
2026-04-17 16:34:10 +0200

    added new projects and slideshow animation added

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
 // toggle icon navbar
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 // scroll sections
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
 
 window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 100;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+      // active sections for animation on scroll
+      sec.classList.add("show-animate");
+    } else {
+      sec.classList.remove("show-animate");
+    }
+  });
 
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop - 100;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');   
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-            // active sections for animation on scroll
-            sec.classList.add('show-animate');
-        }else{
-            sec.classList.remove('show-animate');
-        }
-    }); 
+  // sticky header
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
 
-    // sticky header
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    // remove toggle icon and navbar when click navbar links (scroll)
-    menuIcon.classList.remove('bx-x');
-    navbar.classList.remove('active');
-
-}
+  // remove toggle icon and navbar when click navbar links (scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+};
 
 const projectImages = {
   lednlux: [
@@ -86,7 +86,31 @@ const projectImages = {
     "Images/Project-Images/Energy-Sector/EnergySector6.png",
     "Images/Project-Images/Energy-Sector/EnergySector7.png",
     "Images/Project-Images/Energy-Sector/EnergySectorExtra.png",
-  ]
+  ],
+
+  lednluxVr: [
+    "Images/Project-Images/LednLuxVr/lednlux1.png",
+    "Images/Project-Images/LednLuxVr/lednlux2.png",
+    "Images/Project-Images/LednLuxVr/lednlux3.png",
+    "Images/Project-Images/LednLuxVr/lednlux4.png",
+    "Images/Project-Images/LednLuxVr/lednlux5.png",
+    "Images/Project-Images/LednLuxVr/lednlux6.png",
+  ],
+
+  agri: [
+    "Images/Project-Images/Agri/AgruKubota1.png",
+    "Images/Project-Images/Agri/AgruKubota2.png",
+    "Images/Project-Images/Agri/AgruKubota3.png",
+    "Images/Project-Images/Agri/AgruKubota4.png",
+  ],
+
+  logistictraining: [
+    "Images/Project-Images/Logis/Logis1.png",
+    "Images/Project-Images/Logis/Logis2.png",
+    "Images/Project-Images/Logis/Logis3.png",
+    "Images/Project-Images/Logis/Logis4.png",
+    "Images/Project-Images/Logis/Logis5.png",
+  ],
 };
 
 const slideshows = document.querySelectorAll(".project-slideshow");
@@ -98,15 +122,15 @@ slideshows.forEach((img) => {
   let index = 0;
 
   setInterval(() => {
-    if (!images || images.length === 0) return;
-
-    // img.style.opacity = 0;
+    img.style.opacity = 0;
+    img.style.transform = "scale(1.03)";
 
     setTimeout(() => {
       index = (index + 1) % images.length;
       img.src = images[index];
-    //   img.style.opacity = 1;
-    }, 300);
 
-  }, 2000);
+      img.style.opacity = 1;
+      img.style.transform = "scale(1)";
+    }, 400);
+  }, 3000);
 });
